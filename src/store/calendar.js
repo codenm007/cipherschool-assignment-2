8a011313af5df8ee3e39a15705916c48f07040b5
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-05-15 00:10:27 +0530

    week problem fixed

--- src/store/calendar.js
+++ src/store/calendar.js
@@ -12,12 +12,12 @@ const day_slice = createSlice({
     reducers:{
         prev_week(state){
             const today = new Date(state.start_date);
-            state.start_date=new Date(today.getFullYear(), today.getMonth(), today.getDate()-6).toISOString().slice(0,10);
+            state.start_date=new Date(today.getFullYear(), today.getMonth(), today.getDate()-7).toISOString();
             console.log('inside reducer',state.start_date , 'today' , today);
         },
         next_week(state){
             const today =new Date(state.start_date);
-            state.start_date=new Date(today.getFullYear(), today.getMonth(), today.getDate()+8).toISOString().slice(0,10);
+            state.start_date=new Date(today.getFullYear(), today.getMonth(), today.getDate()+7).toISOString();
             console.log('inside reducer',state.start_date , 'today' , today);
         }
     }
