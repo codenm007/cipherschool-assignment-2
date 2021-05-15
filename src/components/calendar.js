b291dedde99f7e8f9874dda5839d7214bb8f3bcd
Nilanjan Majumdar <n.majumder14@gmail.com>
2021-05-15 22:31:36 +0530

    solved table  arrangement problem

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -2,7 +2,12 @@ import "./calendar.css";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { day_actions } from "../store/calendar";
-import { CheckCircle, Trash,SkipBackwardCircle,SkipForwardCircle } from "react-bootstrap-icons";
+import {
+  CheckCircle,
+  Trash,
+  SkipBackwardCircle,
+  SkipForwardCircle,
+} from "react-bootstrap-icons";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -17,6 +22,15 @@ Date.prototype.getWeek = function () {
   );
 };
 
+//function to check same day
+const sameDay = (d1, d2) => {
+  return (
+    d1.getFullYear() === d2.getFullYear() &&
+    d1.getMonth() === d2.getMonth() &&
+    d1.getDate() === d2.getDate()
+  );
+};
+
 // function which give us the weekly dates with appropiate classes
 let get_week_details = (date) => {
   let week = new Date(new Date(date).toISOString().slice(0, 10)).getWeek();
@@ -28,19 +42,19 @@ let get_week_details = (date) => {
     //console.log(days[new Date(curr.setDate(i)).getDay()]);
     let day_name = days[new Date(j).getDay()];
     let day = new Date(j);
-    let iso_day = day.toISOString().slice(0, 10);
     let time = day.getTime();
     let date = day.getDate();
 
     let day_check = "prev";
     //console.log(time , "time")
-    if (new Date().toISOString().slice(0, 10) == iso_day) {
+    if (sameDay(new Date(), day)) {
       day_check = "today";
     }
     if (new Date().getTime() < time) {
       day_check = "next";
     }
     current_week.push({
+      date_ins: day,
       day: day_name,
       date: date,
       day_check: day_check,
@@ -61,8 +75,6 @@ const Calendar = () => {
   console.log(event_arr);
   const [show_table, set_show_table] = useState(false);
   const [Todo_date, setTodo_date] = useState(new Date());
-  let count = 0;
-  let sun = [] , mon = [] , tue = [] , wed = [] ,thu = [] ,fri = [] , sat = [] ;
 
   useEffect(() => {
     week_arr = get_week_details(day);
@@ -81,187 +93,160 @@ const Calendar = () => {
     //console.log(new Date(day), "after coming to component");
   };
 
-  let day_sorter = (day) => {
-    event_arr.map(j => {
-      let week_days = new Date(day).getWeek();
-      //console.log(week_days[6].toISOString(),count)
-      if(new Date(j.created_at).getDate() === new Date(week_days[0]).getDate()){ //detect event in current weeks sunday
-        sun.push(j);
-        console.log("detected sunday")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[1]).getDate()){ //detect event in current weeks monday
-        mon.push(j);
-        console.log("detected monday")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[2]).getDate()){ //detect event in current weeks tuesday
-        tue.push(j);
-        console.log("detected tue")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[3]).getDate()){ //detect event in current weeks wednesday
-        wed.push(j);
-        console.log("detected wed")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[4]).getDate()){ //detect event in current weeks thurs
-        thu.push(j);
-        console.log("detected thu")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[5]).getDate()){ //detect event in current weeks friday
-        fri.push(j);
-        console.log("detected fri")
-      }
-      if(new Date(j.created_at).getDate() === new Date(week_days[6]).getDate()){ //detect event in current weeks saturday
-        sat.push(j);
-        console.log("detected sat")
-        count++;
-      }
-      console.log(new Date(day).getWeek(),count,"saturday sorted",sat,"sunday_sorted",sun)
-    })
-  }
+  //day_sorter(day);
 
-  
-
+  //console.log(sat);
 
   return (
     <div className="container my-4 ">
       {show_table && (
-        <table className="table table-hover table-bordered">
-          <thead>
-            <tr>
-              {week_arr.map((j) => {
-                let day_class_name = "day_name";
-                let date_class_name = "prev_date";
-                if (j.day_check === "today") {
-                  day_class_name = "today_day_name";
-                  date_class_name = "today_date";
-                }
-                if (j.day_check === "next") {
-                  date_class_name = "next_date";
-                }
-
-                return (
-                  <th scope="col">
-                    <div className="d-flex justify-content-center">
-                      <div className={day_class_name}> {j.day} </div>
-                      <span className={date_class_name}>{j.date}</span>
-                    </div>
-                  </th>
-                );
-              })}
-            </tr>
-          </thead>
-          <tbody>
-            {//calling day sorter
-              day_sorter(day)
+        <div>
+          <table>
+            {week_arr.map((j) => {
+              let day_class_name = "day_name";
+              let date_class_name = "prev_date";
+              let count = 0;
+              if (j.day_check === "today") {
+                day_class_name = "today_day_name";
+                date_class_name = "today_date";
               }
-               <tr>
-              <td>
-                <div
-                  className="done_todo"
-                  onClick={() => {
-                    console.log("hello");
-                  }}
-                >
-                  Water plants
-                </div>
-              </td>
-              <td>
-                <div className="row justify-content-center ">
-                  <div className="col-12 col-md-4 ">
-                    <CheckCircle color="green" size="1.4rem">
-                      {" "}
-                    </CheckCircle>
-                  </div>
-                  <div className="col-12 col-md-4 ">
-                    <p>Study AWS</p>
-                  </div>
-                  <div className="col-12 col-md-4 ">
-                    <Trash color="red" size="1.4rem">
-                      {" "}
-                    </Trash>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  className="btn btn-warning"
-                  data-bs-toggle="modal"
-                  data-bs-target="#staticBackdrop"
-                >
-                  Add Item
-                </button>
+              if (j.day_check === "next") {
+                date_class_name = "next_date";
+              }
 
-                <div
-                  className="modal fade"
-                  id="staticBackdrop"
-                  data-bs-backdrop="static"
-                  data-bs-keyboard="false"
-                  tabindex="-1"
-                  aria-labelledby="staticBackdropLabel"
-                  aria-hidden="true"
-                >
-                  <div className="modal-dialog">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="staticBackdropLabel">
-                          ADD TASK
-                        </h5>
-                        <button
-                          type="button"
-                          className="btn-close"
-                          data-bs-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
+              let todo_print = [];
+              const add_a_task = () => {
+                if (j.day_check !== "prev") {
+                  return (
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-warning"
+                        data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"
+                      >
+                        Add Item
+                      </button>
+
+                      <div
+                        className="modal fade"
+                        id="staticBackdrop"
+                        data-bs-backdrop="static"
+                        data-bs-keyboard="false"
+                        tabindex="-1"
+                        aria-labelledby="staticBackdropLabel"
+                        aria-hidden="true"
+                      >
+                        <div className="modal-dialog">
+                          <div className="modal-content">
+                            <div className="modal-header">
+                              <h5
+                                className="modal-title"
+                                id="staticBackdropLabel"
+                              >
+                                ADD TASK
+                              </h5>
+                              <button
+                                type="button"
+                                className="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                              ></button>
+                            </div>
+                            <div className="modal-body">
+                              <label className="form-label">
+                                Task Description
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Work To Do"
+                              />
+                              <div className="my-3">
+                                <label className="mx-3">
+                                  {" "}
+                                  Please select Todo Date:{" "}
+                                </label>
+                                <DatePicker
+                                  selected={Todo_date}
+                                  onChange={(date) => setTodo_date(date)}
+                                  minDate={new Date()}
+                                  showDisabledMonthNavigation
+                                />
+                              </div>
+                            </div>
+                            <div className="modal-footer">
+                              <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                              >
+                                Close
+                              </button>
+                              <button type="button" className="btn btn-primary">
+                                Add Task
+                              </button>
+                            </div>
+                          </div>
+                        </div>
                       </div>
-                      <div className="modal-body">
-                      <label className="form-label">Task Description</label>
-                        <input
-                          type="text"
-                          className="form-control"
-                          placeholder="Work To Do"
-                        />
-                        <div className = "my-3">
-                          <label className ="mx-3"> Please select Todo Date: </label>
-                        <DatePicker
-                        selected={Todo_date}
-                        onChange={(date) => setTodo_date(date)}
-                        minDate={new Date()}
-                        showDisabledMonthNavigation
-                      />
+                    </td>
+                  );
+                } else {
+                  return <td>chh </td>;
+                }
+              };
+
+              event_arr.map((k) => {
+                if (sameDay(new Date(k.created_at), new Date(j.date_ins))) {
+                  //checking if any todo mateches with current dates
+                  todo_print.push(
+                    <td>
+                      {
+                        <div className="row justify-content-center px-4">
+                          <div className="col-12 col-md-2 ">
+                            <CheckCircle color="green" size="1.4rem">
+                              {" "}
+                            </CheckCircle>
+                          </div>
+                          <div className="col-12 col-md-7">
+                            <p>{k.description}</p>
+                          </div>
+                          <div className="col-12 col-md-3">
+                            <Trash color="red" size="1.4rem">
+                              {" "}
+                            </Trash>
+                          </div>
+                        </div>
+                      }
+                    </td>
+                  );
+                  count++;
+                } else {
+                  todo_print.push(<td>{" sddsds"}</td>);
+                  count++;
+                }
+              });
+
+              if (todo_print.length === count) {
+                return (
+                  <tr>
+                    <th>
+                      <div className="d-flex justify-content-center">
+                        <div className={day_class_name}> {j.day} </div>
+                        <span className={date_class_name}>{j.date}</span>
                       </div>
-                      </div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          data-bs-dismiss="modal"
-                        >
-                          Close
-                        </button>
-                        <button type="button" className="btn btn-primary">
-                          Add Task
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </td>
-              <td>@mdo (sort according to created at )</td>
-              <td>@mdo (sort according to created at )</td>
-              <td>@mdo (sort according to created at )</td>
-              <td>@mdo (sort according to created at )</td>
-            </tr>
-            <tr>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </table>
+                    </th>
+                    {todo_print}
+                    {add_a_task()}
+                  </tr>
+                );
+              }
+
+              //console.log(day_to_print,"dty")
+            })}
+          </table>
+        </div>
       )}
 
       <div className="row my-4">
@@ -271,9 +256,10 @@ const Calendar = () => {
             onClick={() => handle_prev_week()}
           >
             {" "}
-            
-            <SkipBackwardCircle color="white" size="1.4rem" ></SkipBackwardCircle>
-            
+            <SkipBackwardCircle
+              color="white"
+              size="1.4rem"
+            ></SkipBackwardCircle>
           </button>
         </div>
 
@@ -283,9 +269,7 @@ const Calendar = () => {
             onClick={() => handle_next_week()}
           >
             {" "}
-            
             <SkipForwardCircle color="white" size="1.4rem"></SkipForwardCircle>
-           
           </button>
         </div>
       </div>
